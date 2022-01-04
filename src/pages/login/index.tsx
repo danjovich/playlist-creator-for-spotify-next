6a9b9c95f4eae13d724d674b99030555e7ac87e4
Daniel <danjovich@gmail.com>
2022-01-04 14:02:02 -0300

    fix: use getServerSideProps to avoid SS and CS .env.local discrepancies on login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import Login from 'templates/Login';
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage
+} from 'next';
 
-const LoginPage: React.FC = () => {
-  return <Login />;
+import Login from 'templates/Login';
+import { loginUrl } from 'config/loginUrl';
+
+const LoginPage: NextPage = ({
+  loginUrl: url
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return <Login loginUrl={url} />;
 };
 
 export default LoginPage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      loginUrl
+    }
+  };
+};
--- src/templates/Login/index.tsx
+++ src/templates/Login/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { loginUrl } from 'config/loginUrl';
-
+import TopBar from 'components/TopBar';
+import LinkButton from 'components/LinkButton';
 import * as S from './styles';
 
-const Login: React.FC = () => {
+interface Props {
+  loginUrl: string;
+}
+
+const Login: React.FC<Props> = ({ loginUrl }) => {
   return (
     <S.Container>
-      <S.SpotifyLogo
-        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-        alt="Spotify Logo"
+      <TopBar
+        rightButton={
+          <LinkButton href={loginUrl}>Login with Spotify</LinkButton>
+        }
       />
-      <S.LoginButton href={loginUrl}>Login with Spotify</S.LoginButton>
     </S.Container>
   );
 };
