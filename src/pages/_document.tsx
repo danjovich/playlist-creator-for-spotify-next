84ebf902e6dfc0bc9accf9e92cd515d91c70cabd
Daniel <danjovich@gmail.com>
2022-01-04 13:58:42 -0300

    fix: add _document.tsx to solve SSR and CSR className discrepancies

---
+++ src/pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, { DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets } from '@mui/styles';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const styledSheet = new ServerStyleSheet();
+    const materialSheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            styledSheet.collectStyles(
+              materialSheets.collect(<App {...props} />)
+            )
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {materialSheets.getStyleElement()}
+            {styledSheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      styledSheet.seal();
+    }
+  }
+}
