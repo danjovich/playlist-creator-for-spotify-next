416c19ccae33220fb96767e130865a269f711b80
Daniel <danjovich@gmail.com>
2022-01-04 13:55:02 -0300

    add: TopBar component

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,27 @@
+import { MenuOutlined } from '@mui/icons-material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import React, { ReactNode } from 'react';
+
+// import { Container } from './styles';
+
+interface Props {
+  rightButton: ReactNode;
+}
+
+const TopBar: React.FC<Props> = ({ rightButton }) => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton>
+          <MenuOutlined />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Spotify Playlist Creator
+        </Typography>
+        {rightButton}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default TopBar;
