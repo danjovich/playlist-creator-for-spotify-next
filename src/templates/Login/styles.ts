2b518a0ab2c79595152f8832b173ac562d2bdad1
Daniel <danjovich@gmail.com>
2022-01-04 14:02:50 -0300

    chore: remove LoginButton component which is no longer needed

--- src/templates/Login/styles.ts
+++ src/templates/Login/styles.ts
@@ -1,27 +1,12 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: grid;
+  /* display: grid;
   place-items: center;
   height: 100vh;
-  background-color: black;
+  background-color: black; */
 `;
 
 export const SpotifyLogo = styled.img`
   width: 50%;
-`;
-
-export const LoginButton = styled.a`
-  padding: 20px;
-  border-radius: 99px;
-  background-color: #1db954;
-  font-weight: 600;
-  color: white;
-  text-decoration: none;
-
-  &:hover {
-    background-color: white;
-    border-color: #1db954;
-    color: #1db954;
-  }
 `;
